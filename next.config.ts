b3a53bd1b85897a011622e2a2fc3ac51148721ec
Niwat Rungrittidech <niwatnet1979@gmail.com>
2025-11-27 07:21:54 +0700

    Update basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // บังคับให้ Build เป็น HTML Static File
   images: {
-    unoptimized: true, // จำเป็นสำหรับ GitHub Pages (เพราะไม่มี Image Optimization Server)
+    unoptimized: true,
   },
-  // ถ้าคุณ Deploy ไปที่ https://username.github.io/168InteriorLighting/
-  // ต้องใส่ basePath: '/168InteriorLighting'
-  // แต่ถ้าใช้ Custom Domain หรือ Root Domain ไม่ต้องใส่
-  // เดี๋ยวเราลองแบบไม่ใส่ก่อน ถ้า CSS เพี้ยนค่อยมาแก้ครับ
+  basePath: '/168InteriorLighting', // ระบุ Path ของ GitHub Pages
 };
 
 export default nextConfig;
